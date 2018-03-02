e3fe49ac8323c6ff8c7a13a2df91b8d03bc9e72c
Kerry Regan <kregan89@gmail.com>
2018-03-02 14:19:33 -0800

    first commit

--- motionTutorial2/ViewController.swift
+++ motionTutorial2/ViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import CoreMotion
+
 
 class ViewController: UIViewController {
 
+    var motionManager: CMMotionManager!
+    var isRed: Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.becomeFirstResponder()
     }
+    override var canBecomeFirstResponder: Bool { return true }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if(event?.subtype == UIEventSubtype.motionShake) {
+            if isRed {
+                view.backgroundColor = .blue
+                isRed = false
+            }
+            else {
+                view.backgroundColor = .red
+                isRed = true
+            }
+            
+        }
+    }
+
 
 
 }
